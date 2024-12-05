2730a26ea0dac32e263e5ac67c337deabccd326c
JosephMina23 <josephmesias2000@gmail.com>
2024-12-05 14:46:11 -0500

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*const $BTNform = document.querySelector("#form-h2");
+const $form = document.querySelector("#form");
+
+function abrir(){
+    $form.classList.toggle("ocultar");
+}
+
+$BTNform.addEventListener('click', abrir);*/
+
+const $BTNform = document.querySelector("#form-h2");
+const $form = document.querySelector("#form");
+
+function abrir(){
+    $form.classList.toggle("ocultar");
+}
+
+$BTNform.addEventListener('click', abrir)
