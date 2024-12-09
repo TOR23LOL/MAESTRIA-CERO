cd32f43f05fc23df0048e945f0b56cb9db668c72
JosephMina23 <josephmesias2000@gmail.com>
2024-12-09 11:02:30 -0500

    Fixes y boton 'Cuenta tu historia'

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-/*const $BTNform = document.querySelector("#form-h2");
-const $form = document.querySelector("#form");
-
-function abrir(){
-    $form.classList.toggle("ocultar");
-}
-
-$BTNform.addEventListener('click', abrir);*/
-
 const $BTNform = document.querySelector("#form-h2");
 const $form = document.querySelector("#form");
 
